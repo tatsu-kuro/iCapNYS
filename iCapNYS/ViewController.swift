847aa34a21639ad78570c1b0fa2586fbd2e54408
kuroken <kuroken@mac.com>
2020-12-07 20:10:42 +0900

    とりあえずリストから選択して画面遷移まで
    viewの生成のところがまだうまくいっておらず、ボタンが表示されない。
    （ボタン自体は存在していて、反応もする）
    とりあえず望み通りの動作をするようにはなった。
    
    *プロジェクトファイルまでコミットしてしまったのでrevertして再コミット

--- iCapNYS/ViewController.swift
+++ iCapNYS/ViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by 黒田建彰 on 2020/09/21.
 //
-
 //
 //  ViewController.swift
 //  iCapNYS
@@ -13,20 +12,21 @@
 //
 import UIKit
 import Photos
-import AssetsLibrary
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
     let TempFilePath: String = "\(NSTemporaryDirectory())temp.mp4"
     let TODO = ["abd","cddd","nanda"]
-    var videoDate = Array<String>()
+    
+    var videoAssets:PHFetchResult<PHAsset>?
+    
     @IBOutlet weak var how2Button: UIButton!
     @IBOutlet weak var cameraButton: UIButton!
     @IBAction func unwindAction(segue: UIStoryboardSegue) {
         if segue.identifier=="fromRecord"{
             //            print("同じ")
         }
-//        setPlayButtonEnable()//tempFilePathがあれば有効化
+        //        setPlayButtonEnable()//tempFilePathがあれば有効化
         UIApplication.shared.isIdleTimerDisabled = false//スリープする
         if let vc = segue.source as? RecordViewController{
             let Controller:RecordViewController = vc
@@ -37,37 +37,33 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
     //アルバムの一覧表示
     func getAlbumList(){
-   //     let imgManager = PHImageManager.default()
-        videoDate.removeAll()
+        //     let imgManager = PHImageManager.default()
         let requestOptions = PHImageRequestOptions()
         requestOptions.isSynchronous = true
         requestOptions.deliveryMode = .highQualityFormat
         
-        let fetchOptions = PHFetchOptions()
-        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        // "iCapNYS"という名前のアルバムをフェッチ
+        let assetFetchOptions = PHFetchOptions()
+        assetFetchOptions.predicate = NSPredicate(format: "title == %@", "iCapNYS")
+        let assetCollections = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .smartAlbumVideos, options: assetFetchOptions)
         
-        // アルバムをフェッチ
-        let assetCollections = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .smartAlbumVideos, options: nil)//<- Error returned frm daemon:
-        
-        assetCollections.enumerateObjects { assetCollection, _, _ in
+        //アルバムが存在しない事もある？
+        if (assetCollections.count > 0) {
+            //同じ名前のアルバムは一つしかないはずなので最初のオブジェクトを使用
+            let assetCollection = assetCollections.object(at:0)
             
-            // アルバムタイトル
-//            print(assetCollection.localizedTitle ?? "")
-            
-            // アセットをフェッチ
+            // creationDate降順でアルバム内のアセットをフェッチ
+            let fetchOptions = PHFetchOptions()
+            fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
             let assets = PHAsset.fetchAssets(in: assetCollection, options: fetchOptions)
-            var cnt:Int=0
+            videoAssets = assets
+            
+            var cnt:Int = 0
             assets.enumerateObjects { [self] asset, _, _ in
-               
-                
+                //videoAssets.append(asset)
                 if assetCollection.localizedTitle == "iCapNYS"{
                     cnt += 1
-                    //print(asset.creationDate as Any,cnt)
-                    let str = asset.creationDate!.description//2020-11-24 09:29:43 +0000
-                    let str1 = str.components(separatedBy: " +")
-                    let str2 = cnt.description + ") " + str1[0]
-                    videoDate.append(str2)//asset.creationDate!.description)
-                    print(str2)
+                    
                 }
                 // 画像のリクエスト
                 //                imgManager.requestImage(for: asset, targetSize: CGSize(width: 200, height: 200), contentMode:
@@ -82,7 +78,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
     //     if assetCollection.localizedTitle == "アルバム１"{
     //     imageArray.append(img)
-
+    
     func camera_alert(){
         if PHPhotoLibrary.authorizationStatus() != .authorized {
             PHPhotoLibrary.requestAuthorization { status in
@@ -95,21 +91,21 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
             // フォトライブラリに写真を保存するなど、実施したいことをここに書く
         }
     }
-//    func setPlayButtonEnable(){
-//        let checkValidation = FileManager.default
-//        if (checkValidation.fileExists(atPath: TempFilePath)){
-//            playButton.setTitle("［ 再生 ］\n\n最新映像を再生します.\n\n眼振録画映像は\n\niCapNYSアルバムに\n\n保存されます.", for: .normal)
-//            print("ファイルあり、FILE AVAILABLE");
-//            playButton.isEnabled=true
-//        }else{
-//            playButton.setTitle("［ 再生 ］\n\nまだ映像がありません！\n\n眼振録画映像は\n\niCapNYSアルバムに\n\n保存されます.", for: .normal)
-//            print("ファイル無し、FILE NOT AVAILABLE");
-//            playButton.isEnabled=false
-//        }
-//    }
+    //    func setPlayButtonEnable(){
+    //        let checkValidation = FileManager.default
+    //        if (checkValidation.fileExists(atPath: TempFilePath)){
+    //            playButton.setTitle("［ 再生 ］\n\n最新映像を再生します.\n\n眼振録画映像は\n\niCapNYSアルバムに\n\n保存されます.", for: .normal)
+    //            print("ファイルあり、FILE AVAILABLE");
+    //            playButton.isEnabled=true
+    //        }else{
+    //            playButton.setTitle("［ 再生 ］\n\nまだ映像がありません！\n\n眼振録画映像は\n\niCapNYSアルバムに\n\n保存されます.", for: .normal)
+    //            print("ファイル無し、FILE NOT AVAILABLE");
+    //            playButton.isEnabled=false
+    //        }
+    //    }
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.view.backgroundColor = UIColor.white// .cyanColor()
+        //        self.view.backgroundColor = UIColor.white// .cyanColor()
         camera_alert()
         let vw=view.bounds.width
         let bw=vw/4
@@ -130,13 +126,13 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         cameraButton.titleLabel!.textAlignment = NSTextAlignment.center
         cameraButton.setTitle("［ 録画 ］\n\nLEDは光量調節が出来ます.\n\n 明るすぎる機種の場合は\n\nLEDを紙等で覆って\n\n光量を調節して下さい.\n\nKuroda ENT Clinic", for: .normal)
         
-//        playButton.setTitleColor(.white, for: .normal)
-//        playButton.backgroundColor=UIColor.systemGreen
-//        playButton.layer.cornerRadius = 30
-//        playButton.titleLabel!.lineBreakMode = NSLineBreakMode.byWordWrapping
-//        playButton.titleLabel!.numberOfLines = 9
-//        playButton.titleLabel!.textAlignment = NSTextAlignment.center
-//        setPlayButtonEnable()
+        //        playButton.setTitleColor(.white, for: .normal)
+        //        playButton.backgroundColor=UIColor.systemGreen
+        //        playButton.layer.cornerRadius = 30
+        //        playButton.titleLabel!.lineBreakMode = NSLineBreakMode.byWordWrapping
+        //        playButton.titleLabel!.numberOfLines = 9
+        //        playButton.titleLabel!.textAlignment = NSTextAlignment.center
+        //        setPlayButtonEnable()
         UIApplication.shared.isIdleTimerDisabled = false//スリープする
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -145,22 +141,25 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
     //    override func viewDidAppear(_ animated: Bool) {
     //    }
- 
+    
     //nuber of cell
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//最初に一回通る        print("collection -> int")
-        return videoDate.count
+        //最初に一回通る        print("collection -> int")
+        return videoAssets!.count
     }
     //set data on cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell{
         let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier:"cell",for :indexPath)
-        cell.textLabel!.text = videoDate[indexPath.row]
+        
+        cell.textLabel!.text = indexPath.row.description + ") " +
+            videoAssets!.object(at: indexPath.row).creationDate!.description.components(separatedBy:" +")[0]
+        
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(videoDate[indexPath.row])
         let storyboard: UIStoryboard = self.storyboard!
         let nextView = storyboard.instantiateViewController(withIdentifier: "playView") as! PlayViewController
+        nextView.pHAsset = videoAssets!.object(at: indexPath.row)
         self.present(nextView, animated: true, completion: nil)
-       }
+    }
 }
