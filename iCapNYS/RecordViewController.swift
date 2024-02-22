d6b678c1cbaa2818220a0442b5f0a08cc1ecf280
tatsuaki <tatsuakikuroda@me.com>
2024-02-23 01:07:56 +0900

    change top page

--- iCapNYS/RecordViewController.swift
+++ iCapNYS/RecordViewController.swift
@@ -804,7 +804,7 @@ class RecordViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         LEDValueLabel.isHidden=false
         cameraView.isHidden=false
         quaternionView.isHidden=false
-   //     bleButton.isHidden=true
+        bleButton.isHidden=true
 
 //print("setteimode:******:",setteiMode)
         if cameraType==0{
@@ -828,7 +828,7 @@ class RecordViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
             enterButton.isHidden=false
             urlLabel.isHidden=false
             urlInputField.isHidden=false
-  //          bleButton.isHidden=false
+            bleButton.isHidden=false
             LEDBar.isHidden=true
             LEDLabel.isHidden=true
             LEDValueLabel.isHidden=true
@@ -1062,9 +1062,9 @@ class RecordViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         camera.setLabelProperty(focusLabel,x:x0,y:by,w:bw,h:bh,UIColor.white)
         focusBar.frame = CGRect(x:x0+bw+sp, y: by, width:bw*2+sp, height: bh)
         camera.setButtonProperty(bleButton, x: x0, y: by1, w: bw*2, h: bh, UIColor.darkGray,0)
-    //    bleButton.alpha=0.02
-        bleButton.setTitle("Send", for: .normal)
-        bleButton.isHidden=true
+//        bleButton.alpha=0.02
+        bleButton.setTitle("Send Motion Data", for: .normal)
+//        bleButton.isHidden=true
         camera.setLabelProperty(LEDLabel,x:x0,y:by1,w:bw,h:bh,UIColor.white)
         LEDBar.frame = CGRect(x:x0+bw+sp,y:by1,width:bw*2+sp,height:bh)
         
