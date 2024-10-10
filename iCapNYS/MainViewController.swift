ffd52c0ae8f2cd8bfe6a0d24d1ad32a58b37b297
tatsuaki <tatsuakikuroda@me.com>
2024-10-10 22:37:07 +0900

    delete gomi

--- iCapNYS/MainViewController.swift
+++ iCapNYS/MainViewController.swift
@@ -175,208 +175,9 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     override var prefersHomeIndicatorAutoHidden: Bool {
         return true
     }
-    //これは.isHiddenとする
- //   @IBOutlet weak var //Auto: UIButton!
-    @IBOutlet weak var returnButton: UIButton!
- //   @IBOutlet weak var positioningAutoRecordButton: UIButton!
-    
-//    override var shouldAutorotate: Bool {
-//        return false
-//    }
-//
-//    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-//        let landscapeSide=someFunctions.getUserDefaultInt(str: "landscapeSide", ret: 0)
-//        if landscapeSide==0{
-//            return UIInterfaceOrientationMask.landscapeRight
-//        }else{
-//            return UIInterfaceOrientationMask.landscapeLeft
-//        }
-//    }
-    //setteiMode 0:Camera 1:manual_settei(green) 2:auto_settei(orange)
-//    @IBAction func onCameraButton(_ sender: Any) {
-//        checkLibraryAuthorized()
-//        if authorizedFlag != 2{
-//            print("not authorized!!!")
-//            alertNotAuthorized()
-//            return
-//        }
-// //       stopMotion()
-//        UserDefaults.standard.set(UIScreen.main.brightness, forKey: "brightness")
-//        let cameraType=someFunctions.getUserDefaultInt(str: "cameraType", ret: 0)
-//        let frontCameraMode=someFunctions.getUserDefaultInt(str:"frontCameraMode",ret: 0)
-//        if cameraType==0 && frontCameraMode==1{
-//   //         onAutoRecordButton(0)
-//            return
-//        }else if cameraType==0 && frontCameraMode==2{
-//            onPositioningRecordButton(0)
-//            return
-//        }
-//        if cameraType==5{
-//            let nextView1 = storyboard?.instantiateViewController(withIdentifier: "WIFI") as! WifiViewController
-//            self.present(nextView1, animated: true, completion: nil)
-//        }else{
-//            let nextView = storyboard?.instantiateViewController(withIdentifier: "RECORD") as! RecordViewController
-//            nextView.setteiMode=0
-//            nextView.autoRecordMode=false
-//            self.present(nextView, animated: true, completion: nil)
-//        }
-//    }
-//    
-//    @IBAction func onSetteiButtonAuto(_ sender: Any) {
-// 
-////        checkLibraryAuthorized()
-////        if authorizedFlag != 2{
-////            print("not authorized!!!",authorizedFlag)
-////            alertNotAuthorized()
-////            return
-////        }
-////        let nextView = storyboard?.instantiateViewController(withIdentifier: "RECORD") as! RecordViewController
-////        nextView.setteiMode=2
-////        nextView.autoRecordMode=false
-////        nextView.explanationLabeltextColor=UIColor.systemOrange
-////        UserDefaults.standard.set(UIScreen.main.brightness, forKey: "brightness")
-////        self.present(nextView, animated: true, completion: nil)
-//    }
-    
-//    @IBAction func onSetteiButtonManual(_ sender: Any) {
-//        checkLibraryAuthorized()
-//        if authorizedFlag != 2{
-//            print("not authorized!!!",authorizedFlag)
-//            alertNotAuthorized()
-//            return
-//        }
-// //       stopMotion()
-//        let nextView = storyboard?.instantiateViewController(withIdentifier: "RECORD") as! RecordViewController
-//        nextView.setteiMode=1
-//        nextView.autoRecordMode=false
-//        nextView.explanationLabeltextColor=UIColor.systemGreen
-//        UserDefaults.standard.set(UIScreen.main.brightness, forKey: "brightness")
-//        self.present(nextView, animated: true, completion: nil)
-//    }
-    
-//    @IBAction func onPositioningRecordButton(_ sender: Any) {
-////        stopMotion()
-//        let nextView = storyboard?.instantiateViewController(withIdentifier: "AUTORECORD") as! AutoRecordViewController
-//        nextView.isPositional=true
-//        UserDefaults.standard.set(UIScreen.main.brightness, forKey: "brightness")//cgfloat-double?
-//        self.present(nextView, animated: true, completion: nil)
-//    }
-//    @IBAction func onAutoRecordButton(_ sender: Any) {
-// //       stopMotion()
-//        let nextView = storyboard?.instantiateViewController(withIdentifier: "AUTORECORD") as! AutoRecordViewController
-//        nextView.isPositional=false
-//        UserDefaults.standard.set(UIScreen.main.brightness, forKey: "brightness")
-//        self.present(nextView, animated: true, completion: nil)
-//    }
-//    @IBAction func onChangeLandscapeSide(_ sender: Any) {
-//        var landscapeSide=someFunctions.getUserDefaultInt(str: "landscapeSide", ret: 0)
-//        if landscapeSide==0{
-//            landscapeSide=1
-//        }else{
-//            landscapeSide=0
-//        }
-//        UserDefaults.standard.set(landscapeSide,forKey: "landscapeSide")
-//        let nextView = storyboard?.instantiateViewController(withIdentifier: "HOW2") as! How2ViewController
-//        self.present(nextView, animated: true, completion: nil)
-//    }
-    
-/*    @IBAction func unwindAction(segue: UIStoryboardSegue) {
-        print("segueWhat:",segue)
-        if let vc = segue.source as? RecordViewController{
-            let Controller:RecordViewController = vc
-            if Controller.stopButton.isHidden==true{//Exit
-                print("Exit / not recorded")
-            }else{
-                print("Exit / recorded")
-                if someFunctions.videoPHAsset.count<5{
-                    someFunctions.getAlbumAssets()
-                    print("count<5")
-                }else{
-                    someFunctions.getAlbumAssets_last()
-                    print("count>4")
-                }
-                UserDefaults.standard.set(0,forKey: "contentOffsetY")
-                DispatchQueue.main.async { [self] in
-                    self.tableView.contentOffset.y=0
-                }
-            }
- 
-            print("segue:","\(segue.identifier!)")
-            Controller.motionManager.stopDeviceMotionUpdates()
-            Controller.captureSession.stopRunning()
-        }else if let vc1 = segue.source as? WifiViewController{
-            let Controller:WifiViewController = vc1
-            if Controller.stopButton.isHidden==true{//Exit
-                print("Exit / not recorded")
-            }else{
-                print("Exit / recorded")
-                if someFunctions.videoPHAsset.count<5{
-                    someFunctions.getAlbumAssets()
-                    print("count<5")
-                }else{
-                    someFunctions.getAlbumAssets_last()
-                    print("count>4")
-                }
-                UserDefaults.standard.set(0,forKey: "contentOffsetY")
-                DispatchQueue.main.async { [self] in
-                    self.tableView.contentOffset.y=0
-                    self.tableView.reloadData()//こちらだけこれが必要なのはどうして
 
-                }
-            }
-   
-            print("segue:","\(segue.identifier!)")
-            Controller.motionManager.stopDeviceMotionUpdates()
-//            Controller.captureSession.stopRunning()
-            
-        }else if let vc = segue.source as? AutoRecordViewController{
-            let Controller:AutoRecordViewController = vc
-            Controller.killTimer()//念の為
-            if (Controller.isPositional==false && Controller.movieTimerCnt>25) ||
-                (Controller.isPositional==true && Controller.movieTimerCnt>112){
-                print("Exit / Auto recorded")
-                if someFunctions.videoPHAsset.count<5{
-                    someFunctions.getAlbumAssets()
-                    print("count<5")
-                }else{
-                    someFunctions.getAlbumAssets_last()
-                    print("count>4")
-                }
-                UserDefaults.standard.set(0,forKey: "contentOffsetY")
-                DispatchQueue.main.async { [self] in
-                    self.tableView.contentOffset.y=0
-                    self.tableView.reloadData()//こちらだけこれが必要なのはどうして
-                }
-            }
-  
-            print("segue:","\(segue.identifier!)")
-            Controller.motionManager.stopDeviceMotionUpdates()
-            Controller.captureSession.stopRunning()
-        }else if let vc = segue.source as? AutoRecordViewController{
-        }else if let vc = segue.source as? BLEViewController{
-            let Controller:BLEViewController = vc
-            Controller.motionManager.stopDeviceMotionUpdates()
-        }
-        UIScreen.main.brightness = CGFloat(UserDefaults.standard.double(forKey: "brightness"))
-        UIApplication.shared.isIdleTimerDisabled = false//スリープする.監視する
-        print("unwind")
-//        isStarted=false
-//        startMotion()
-    }
-*/
-//    func camera_alert(){
-//        if PHPhotoLibrary.authorizationStatus() != .authorized {
-//            PHPhotoLibrary.requestAuthorization { status in
-//                if status == .authorized {
-//                    // フォトライブラリに写真を保存するなど、実施したいことをここに書く
-//                } else if status == .denied {
-//                }
-//            }
-//        } else {
-//            // フォトライブラリに写真を保存するなど、実施したいことをここに書く
-//        }
-//        
-//    }
+    @IBOutlet weak var returnButton: UIButton!
+
     func alertNotAuthorized(){
         if someFunctions.firstLang().contains("ja"){
             let alert = UIAlertController(title: "写真アクセスエラー", message: "写真へのアクセスが制限されています。設定アプリを起動して、iCapNYSをタップし、写真をフルアクセスに設定して下さい。", preferredStyle: .alert)
@@ -393,35 +194,9 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         print("viewDidLoad*******")
         UserDefaults.standard.set(UIScreen.main.brightness, forKey: "brightness")
 
-  //      isStarted=false
- //       startMotion()
-//        if PHPhotoLibrary.authorizationStatus() != .authorized {
-//            PHPhotoLibrary.requestAuthorization { status in
-//                if status == .authorized {
-//                    self.checkLibraryAuthrizedFlag=1
-//                    print("authorized")
-//                } else if status == .denied {
-//                    self.checkLibraryAuthrizedFlag = -1
-//                    print("denied")
-//                }else{
-//                    self.checkLibraryAuthrizedFlag = -1
-//                }
-//            }
-//        }else{
-        
-//    checkLibraryAuthorized()
-//        while(authorizedFlag == 0 || authorizedFlag == 4){
-//            checkLibraryAuthorized()
-//            print("authorizedFlag:",authorizedFlag)
-//            sleep(UInt32(0.1))
-//        }
-            someFunctions.getAlbumAssets()//完了したら戻ってくるようにしたつもり
-//        }
+        someFunctions.getAlbumAssets()//完了したら戻ってくるようにしたつもり
         //初回起動時にdefaultを設定
         let cameraType=someFunctions.getUserDefaultInt(str: "cameraType", ret: 0)
-//        let topEndBlank=0//someFunctions.getUserDefaultInt(str: "topEndBlank", ret: 0)
-            
-//        UIApplication.shared.isIdleTimerDisabled = false//スリープする
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(foreground(notification:)),
                                                name: UIApplication.willEnterForegroundNotification,
@@ -432,12 +207,8 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
      }
     @objc func foreground(notification: Notification) {
         print("フォアグラウンド")
-//        startMotion()
     }
     override func viewDidAppear(_ animated: Bool) {
-//        if UIApplication.shared.isIdleTimerDisabled == true{//スリープさせない
-//            UIApplication.shared.isIdleTimerDisabled = false//監視する
-//        }
         print("viewDidAppear*********")
         tableView.reloadData()
         let contentOffsetY = CGFloat(someFunctions.getUserDefaultFloat(str:"contentOffsetY",ret:0))
@@ -473,8 +244,6 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         let ww:CGFloat=view.bounds.width-leftPadding-rightPadding
         let wh:CGFloat=view.bounds.height-topPadding-bottomPadding
         let sp=ww/120//間隙
-//        let bw=(ww-sp*10)/7//ボタン幅
-//         let x0=leftPadding+sp*2
         let x0but=view.bounds.width-rightPadding-wh*3/4
         let x1but=x0but+wh/2-wh/40
         let bw=view.bounds.width-x1but-rightPadding-2*sp
@@ -483,58 +252,21 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         let by0=topPadding+sp
         someFunctions.setButtonProperty(returnButton, x:x1but, y: by, w: bw, h: bh, UIColor.darkGray,0)
 
-        let cx=leftPadding+ww-wh*3/4+wh*5/13//-(x1but+sp/2-bw-sp))-cr
-  //      let a=2*cx-x1but-bw
- //             sendButton.frame=CGRect(x:a/*x1but+sp/2-bw-sp*/, y:topPadding+sp, width: cr, height:cr )
-  //      someFunctions.setButtonProperty(how2Button, x:/*a*/x1but+sp/2-bw-sp, y: by, w: bw, h: bh, UIColor.darkGray,0)
+  //      let cx=leftPadding+ww-wh*3/4+wh*5/13//-(x1but+sp/2-bw-sp))-cr
+
         someFunctions.setButtonProperty(how2Button, x:x1but, y:by0, w: bw, h: bh, UIColor.darkGray,0)
         someFunctions.setButtonProperty(gyroButton, x:x1but, y:by0+bh+sp, w: bw, h: bh, UIColor.darkGray,0)
-//        sendButton.isHidden=true
-//        someFunctions.setButtonProperty(how2Button, x:x1but, y:by0, w: bw, h: bh, UIColor.darkGray,0)
- //       autoRecordButton.frame=CGRect(x:x0but,y:sp,width: wh/2,height: wh/2)
- //       positioningAutoRecordButton.frame=CGRect(x:x0but,y:wh/2-sp,width: wh/2,height: wh/2)
-  //      let upCircleX0=sp+wh/4
-  //      let downCircleX0=wh/2-sp+wh/4
- //       steelLabel.frame=CGRect(x:x0but,y:upCircleX0-wh/9-bh/2,width: wh/2,height: bh*3.5)
- //       postualLabel.frame=CGRect(x:x0but,y:downCircleX0-wh/9-bh/2,width: wh/2,height: bh*3.5)
-       //下ボタンを有効にするとLandscapeLeft,Rightを変更可能となる。infoに(left home button),(right home button)両方指定
-//        changeLandscapeSideButton.isHidden=true
+
         //以下2行ではRightに設定。leftに変更するときは、infoにもlandscape(left home button)を設定
         let landscapeSide=0//0:right 1:left
         UserDefaults.standard.set(landscapeSide,forKey: "landscapeSide")
         tableView.frame = CGRect(x:leftPadding,y:topPadding+sp,width: x1but-leftPadding-sp*2,height: wh-sp*2)
-   
-   //     cameraButton.frame=CGRect( x: tableView.frame.maxX, y:topPadding+wh*3/26,width:wh*10/13, height: wh*10/13)
-//        cameraButton.frame=CGRect( x: view.bounds.width-rightPadding-wh*3/4, y:topPadding+wh*3/26,width:wh*10/13, height: wh*10/13)
-        //高さ/20を上下に開ける
-  
-        if someFunctions.firstLang().contains("ja"){
-          //  how2Button.setTitle("使い方", for: .normal)
-          //  setteiButtonAuto.setTitle("録画設定", for: .normal)
-         //   returnButton.setTitle("録画設定", for: .normal)
-         //   steelLabel.text="座って記録\n20秒"
-         //   postualLabel.text="横になって記録\n90秒"
-        }
-  //      postualLabel.isHidden=true
-  //      steelLabel.isHidden=true
-  //      autoRecordButton.isHidden=true
-  //      positioningAutoRecordButton.isHidden=true
- //       setteiButtonAuto.isHidden=true
-    //    cameraButton.isHidden=true
-    //    sendButton.isHidden=true
+ 
     }
-  
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        print("viewWillAppear********")
-//        UIApplication.shared.isIdleTimerDisabled = false//スリープさせる
-//    }
+ 
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        //            // 画面非表示直後の処理を書く
-        //            print("画面非表示直後")
-        //        UserDefaults.standard.set(0,forKey: "contentOffsetY")
-        //
+    
         let contentOffsetY = tableView.contentOffset.y
         print("offset:",contentOffsetY)
         UserDefaults.standard.set(contentOffsetY,forKey: "contentOffsetY")
@@ -553,17 +285,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     func tableView(_ tableView: UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell{
         let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier:"cell",for :indexPath)
         let topEndBlank=0//UserDefaults.standard.integer(forKey:"topEndBlank")
-       /* if topEndBlank==0{
-            let number = (indexPath.row+1).description + ") "
-            cell.textLabel!.text = number + someFunctions.videoDate[indexPath.row]
-        }else{
-            let number = (indexPath.row).description + ") "
-            if indexPath.row==0 || indexPath.row==someFunctions.videoDate.count+1{
-                cell.textLabel!.text = " "
-            }else{
-                cell.textLabel!.text = number + someFunctions.videoDate[indexPath.row-1]
-            }
-        }*/
+     
         var cellText:String=""
         if topEndBlank==0{
             let number = (indexPath.row+1).description + ")"
@@ -613,17 +335,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
              indexPathRow -= 1
             }
         }
-//        let str=someFunctions.videoAlbumAssets[indexPath.row]?.a absoluteString
-//        print(str as Any)
-//        if str!.contains("temp.mp4"){
-//            return
-//        }
-//        return
-//        print("asset:",someFunctions.videoAlbumAssets[indexPath.row])
-//        if someFunctions.videoURL[indexPath.row]==nil{
-//            return
-//        }
-//        print(someFunctions.videoAlbumAssets[indexPath.row])
+
         videoCurrentCount=indexPathRow// indexPath.row
         print("video:",videoCurrentCount)
         let contentOffsetY = tableView.contentOffset.y
@@ -637,7 +349,6 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         let storyboard: UIStoryboard = self.storyboard!
         let nextView = storyboard.instantiateViewController(withIdentifier: "playView") as! PlayViewController
       
-//        nextView.videoURL = someFunctions.videoURL[indexPath.row]
         nextView.phasset = someFunctions.videoPHAsset[indexPathRow]// indexPath.row]
         nextView.avasset = avasset
         nextView.calcDate = someFunctions.videoDate[indexPathRow]
@@ -681,5 +392,4 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
             }
         }
     }
-   
 }
